65b821a94dc091d9bd5de972a7456cd4adb7fa91
abdelkarimanouer <abdelkarimanouer03@gmail.com>
2025-02-13 22:27:32 +0100

    Initial Commit

---
+++ FrmTimerForStudy.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace TimerForStudy
+{
+    partial class frmTimerForStudy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTimerForStudy));
+            this.lbHours = new System.Windows.Forms.Label();
+            this.lbMinutes = new System.Windows.Forms.Label();
+            this.lbSeconds = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbHours
+            // 
+            this.lbHours.AutoSize = true;
+            this.lbHours.Font = new System.Drawing.Font("Lucida Sans Unicode", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbHours.ForeColor = System.Drawing.Color.White;
+            this.lbHours.Location = new System.Drawing.Point(3, 19);
+            this.lbHours.Name = "lbHours";
+            this.lbHours.Size = new System.Drawing.Size(216, 146);
+            this.lbHours.TabIndex = 0;
+            this.lbHours.Text = "00";
+            // 
+            // lbMinutes
+            // 
+            this.lbMinutes.AutoSize = true;
+            this.lbMinutes.Font = new System.Drawing.Font("Lucida Sans Unicode", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMinutes.ForeColor = System.Drawing.Color.White;
+            this.lbMinutes.Location = new System.Drawing.Point(249, 19);
+            this.lbMinutes.Name = "lbMinutes";
+            this.lbMinutes.Size = new System.Drawing.Size(216, 146);
+            this.lbMinutes.TabIndex = 1;
+            this.lbMinutes.Text = "00";
+            // 
+            // lbSeconds
+            // 
+            this.lbSeconds.AutoSize = true;
+            this.lbSeconds.Font = new System.Drawing.Font("Lucida Sans Unicode", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSeconds.ForeColor = System.Drawing.Color.White;
+            this.lbSeconds.Location = new System.Drawing.Point(492, 19);
+            this.lbSeconds.Name = "lbSeconds";
+            this.lbSeconds.Size = new System.Drawing.Size(216, 146);
+            this.lbSeconds.TabIndex = 2;
+            this.lbSeconds.Text = "00";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Lucida Sans Unicode", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(179, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 146);
+            this.label5.TabIndex = 4;
+            this.label5.Text = ":";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Lucida Sans Unicode", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(420, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(101, 146);
+            this.label6.TabIndex = 5;
+            this.label6.Text = ":";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.BackColor = System.Drawing.Color.Black;
+            this.btnStartStop.Font = new System.Drawing.Font("Tahoma", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStartStop.ForeColor = System.Drawing.Color.White;
+            this.btnStartStop.Location = new System.Drawing.Point(380, 192);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(210, 79);
+            this.btnStartStop.TabIndex = 6;
+            this.btnStartStop.Text = "Start";
+            this.btnStartStop.UseVisualStyleBackColor = false;
+            this.btnStartStop.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.Black;
+            this.btnReset.Font = new System.Drawing.Font("Tahoma", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnReset.Location = new System.Drawing.Point(80, 192);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(210, 79);
+            this.btnReset.TabIndex = 7;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // frmTimerForStudy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(708, 303);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStartStop);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbSeconds);
+            this.Controls.Add(this.lbMinutes);
+            this.Controls.Add(this.lbHours);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmTimerForStudy";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Timer For Study";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbHours;
+        private System.Windows.Forms.Label lbMinutes;
+        private System.Windows.Forms.Label lbSeconds;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnStartStop;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ FrmTimerForStudy.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimerForStudy
+{
+    public partial class frmTimerForStudy : Form
+    {
+        private byte _Seconds = 0;
+        private byte _Minutes = 0;
+        private byte _Hours = 0;
+
+        public frmTimerForStudy()
+        {
+            InitializeComponent();
+
+            LoadTimeFromFile();
+        }
+
+        private void SaveTimeInFile()
+        {
+            using (StreamWriter sr = new StreamWriter("D:\\Programmer\\C# Coding\\TimerForStudy\\HistoryTime.txt"))
+            {
+                sr.WriteLine($"{_Hours}:{_Minutes}:{_Seconds}");
+            }
+        }
+
+       private void LoadTimeFromFile()
+       {
+            string filePath = "D:\\Programmer\\C# Coding\\TimerForStudy\\HistoryTime.txt";
+            string timeString = File.ReadAllText(filePath);
+
+            if (string.IsNullOrEmpty(timeString))
+            {
+                _Hours = 0;
+                _Minutes = 0;
+                _Seconds = 0;
+                return;
+            }
+
+            string[] timeParts = timeString.Split(':'); 
+
+            _Hours = byte.Parse(timeParts[0]);
+            _Minutes = byte.Parse(timeParts[1]);
+            _Seconds = byte.Parse(timeParts[2]);
+
+            lbSeconds.Text = _Seconds.ToString("D2");
+            lbMinutes.Text = _Minutes.ToString("D2");
+            lbHours.Text = _Hours.ToString("D2");
+
+        }
+        private void ClearFile()
+        {
+            using (StreamWriter sr = new StreamWriter("D:\\Programmer\\C# Coding\\TimerForStudy\\HistoryTime.txt"))
+            {
+                sr.WriteLine($"{_Hours}:{_Minutes}:{_Seconds}");
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            _Seconds++;
+
+            if (_Seconds == 60)
+            {
+                _Seconds = 0;
+                _Minutes++;
+
+                if (_Minutes == 60)
+                {
+                    _Minutes = 0;
+                    _Hours++;
+                }
+            }
+
+            lbSeconds.Text = _Seconds.ToString("D2");
+            lbMinutes.Text = _Minutes.ToString("D2");
+            lbHours.Text = _Hours.ToString("D2");
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (btnStartStop.Text == "Stop")
+            {
+                timer1.Stop();
+                SaveTimeInFile();
+                btnStartStop.Text = "Start";
+                btnStartStop.ForeColor = Color.White;
+                return;
+            }
+      
+            timer1.Start();
+            btnStartStop.Text = "Stop";
+            btnStartStop.ForeColor = Color.Red;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            btnStartStop.Text = "Start";
+
+            timer1.Stop();
+
+            _Seconds = 0;
+            _Minutes = 0;
+            _Hours = 0;
+
+            lbSeconds.Text = "00";
+            lbMinutes.Text = "00";
+            lbHours.Text = "00";
+
+            ClearFile();
+        }
+    }
+}
